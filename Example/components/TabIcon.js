b0f1e650017a09f79bc6cbf367bb717832476504
philipshurpik <philip.shurpik@gmail.com>
2017-10-31 12:07:38 +0200

    fixed proptypes

--- Example/components/TabIcon.js
+++ Example/components/TabIcon.js
@@ -1,9 +1,8 @@
-import React, {
-  PropTypes,
-} from 'react';
+import React from 'react';
 import {
   Text,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 const propTypes = {
   selected: PropTypes.bool,
